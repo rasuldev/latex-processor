35acfdf69720c74af3697015ce6d7268c942072f
Rasul <rasuldev@gmail.com>
2012-09-14 21:43:06 +0300

    Replaces 2576-blocks that have \eqno{num} by \begin{equation}\label{num} ... \end{equation}

--- TextProcessor/Program.cs
+++ TextProcessor/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -11,26 +9,37 @@ namespace TextProcessor
     {
         static void Main(string[] args)
         {
-            var str = File.ReadAllText("test.txt");
+            ProcessFile(@"..\..\test.txt",@"..\..\test_processed.txt");
+        }
+
+        /// <summary>
+        /// Replaces $$-blocks that have \eqno{num} by 
+        /// \begin{equation}\label{num} ... \end{equation}
+        /// </summary>
+        /// <param name="sourceFilename">File to be processed</param>
+        /// <param name="destinationFilename">Result will be saved in this file</param>
+        static void ProcessFile(string sourceFilename, string destinationFilename)
+        {
+            var str = File.ReadAllText(sourceFilename);
             var sb = new StringBuilder(str);
 
             // Find all occurrences of eqno and extract number
-            var matches = Regex.Matches(str, @"eqno *\{(.+)\}");
+            var matches = Regex.Matches(str, @"\\eqno *\{(.+)\}");
             // Process matches (to avoid changing matching positions during replace we do processing from end to begin) 
-            foreach (Match match in matches.OfType<Match>().OrderByDescending(m=>m.Index))
+            foreach (Match match in matches.OfType<Match>().OrderByDescending(m => m.Index))
             {
-                Console.WriteLine("{0}({1})", match.Groups[1].Value.Trim(), match.Index);
+                //Console.WriteLine("{0}({1})", match.Groups[1].Value.Trim(), match.Index);
 
                 int endDollarsPos = sb.ToString().IndexOf("$$", match.Index);
                 sb = sb.Replace("$$", @"\end{equation}", endDollarsPos, 2);
 
-                int beginDollarsPos = sb.ToString().LastIndexOf("$$", 0, match.Index);
-                sb = sb.Replace("$$", @"\begin{equation}", beginDollarsPos, 2);
-                
-                //Console.WriteLine(str.IndexOf("$$", match.Index));
-                
+                sb = sb.Remove(match.Index, match.Value.Length);
+
+                int beginDollarsPos = sb.ToString().LastIndexOf("$$", match.Index);
+                sb = sb.Replace("$$", @"\begin{equation}\label{" + match.Groups[1].Value.Trim() + "}", beginDollarsPos, 2);
             }
-            Console.WriteLine(sb.ToString());
+
+            File.WriteAllText(destinationFilename,sb.ToString());
         }
     }
 }
