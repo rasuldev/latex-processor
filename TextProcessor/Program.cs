b1e9f284743b05c6369aeb27c0493d97475421f2
Rasul <rasuldev@gmail.com>
2012-09-16 22:29:34 +0300

    added changing formula refs from (52.1) to \eqref{52.1}

--- TextProcessor/Program.cs
+++ TextProcessor/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -29,7 +30,12 @@ namespace TextProcessor
             var sb = new StringBuilder(str);
 
             // Find all occurrences of eqno and extract number
-            var matches = Regex.Matches(str, @"\\eqno *\{(.+)\}");
+            var matches = Regex.Matches(str, @"\\eqno *\((.+)\)");
+
+            // In eqNumbers we will collect equation numbers
+            // It will be used to change formula references from (52.1) to \eqref{52.1}
+            List<string> eqNumbers= new List<string>();
+            
             // Process matches (to avoid changing matching positions during replace we do processing from end to begin) 
             foreach (Match match in matches.OfType<Match>().OrderByDescending(m => m.Index))
             {
@@ -41,10 +47,19 @@ namespace TextProcessor
                 sb = sb.Remove(match.Index, match.Value.Length);
 
                 int beginDollarsPos = sb.ToString().LastIndexOf("$$", match.Index);
-                sb = sb.Replace("$$", @"\begin{equation}\label{" + match.Groups[1].Value.Trim() + "}", beginDollarsPos, 2);
+                var eqNumber = match.Groups[1].Value.Trim();
+                sb = sb.Replace("$$", @"\begin{equation}\label{" + eqNumber + "}", beginDollarsPos, 2);
+                eqNumbers.Add(eqNumber);
             }
 
-            File.WriteAllText(destinationFilename, sb.ToString(), encoding);
+            // Changing formula references
+            string text = sb.ToString();
+            foreach (var eqNumber in eqNumbers)
+            {
+                text = Regex.Replace(text, string.Format(@"\(\s*{0}\s*\)", eqNumber), @"\eqref{"+eqNumber+"}");
+            }
+
+            File.WriteAllText(destinationFilename, text, encoding);
         }
     }
 }
