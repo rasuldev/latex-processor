fef0df7d67f20c2f0797c96e383fbd7a2e6823d5
Rasul <rasuldev@gmail.com>
2012-09-26 18:21:56 +0300

    added converting from \begin{equation} to dollars

--- TextProcessor/Program.cs
+++ TextProcessor/Program.cs
@@ -14,7 +14,8 @@ namespace TextProcessor
             // Choose encoding of your file: default is UTF-8
             // var encoding = Encoding.GetEncoding("windows-1251");
             var encoding = Encoding.GetEncoding("utf-8");
-            ProcessFile(@"..\..\test.txt", @"..\..\test_processed.txt",encoding);
+            //ProcessFile(@"..\..\test.txt", @"..\..\test_processed.txt",encoding);
+            ProcessFile(@"..\..\test_processed.txt", @"..\..\test2.txt", encoding);
         }
 
         /// <summary>
@@ -27,16 +28,58 @@ namespace TextProcessor
         {
             if (encoding == null)
                 encoding = new UTF8Encoding();
-            var str = File.ReadAllText(sourceFilename, encoding);
-            var sb = new StringBuilder(str);
+            var source = File.ReadAllText(sourceFilename, encoding);
+            //var text = MakeEquationWithLabelsFromDollars(source);
+            var text = MakeDollarsFromEquationWithLabels(source);
+            File.WriteAllText(destinationFilename, text, encoding);
+        }
+
+        static string MakeDollarsFromEquationWithLabels(string source)
+        {
+            var sb = new StringBuilder(source);
+
+            // Replacing \begin{equation},\end{equation} with $$
+            sb = sb.Replace(@"\begin{equation}", "$$");
+            sb = sb.Replace(@"\end{equation}", "$$");
+
+            // Harvesting labels, placing eqno() and removing labels
+            var labels = new Stack<string>();
+            var matches = Regex.Matches(sb.ToString(), @"\\label(?:.|\r?\n)*?\{(.+?)\}");
+            // Labels count
+            var eqNumber = matches.Count;
+
+            // Enumeration is started from end.
+            // So last equation number would be equal to labels count
+            foreach (Match match in matches.OfType<Match>().OrderByDescending(m => m.Index))
+            {
+                labels.Push(match.Groups[1].Value);
+                var endDollarsPos = sb.ToString().IndexOf("$$", match.Index);
+                sb = sb.Insert(endDollarsPos, @"\eqno(" + eqNumber + ")\r\n");
+                eqNumber--;
+                sb = sb.Remove(match.Index, match.Length);
+            }
+
+            // Replacing \eqref 
+            var labelsArr = labels.ToArray();
+            for (int i = 0; i < labelsArr.Length; i++)
+            {
+                sb = sb.Replace(@"\eqref{" + labelsArr[i] + @"}", "(" + (i + 1) + ")");
+            }
+
+            return sb.ToString();
+        }
+
+        static string MakeEquationWithLabelsFromDollars(string source)
+        {
+            var sb = new StringBuilder(source);
 
             // Find all occurrences of eqno and extract number
-            var matches = Regex.Matches(str, @"\\eqno(?:.|\r?\n)*?\((.+?)\)");
+            var matches = Regex.Matches(source, @"\\eqno(?:.|\r?\n)*?\((.+?)\)");
 
             // In eqNumbers we will collect equation numbers
             // It will be used to change formula references from (52.1) to \eqref{52.1}
-            List<string> eqNumbers= new List<string>();
-            
+            List<string> eqNumbers = new List<string>();
+
             // Process matches (to avoid changing matching positions during replace we do processing from end to begin) 
             foreach (Match match in matches.OfType<Match>().OrderByDescending(m => m.Index))
             {
@@ -55,13 +98,13 @@ namespace TextProcessor
 
             // Changing formula references
             string text = sb.ToString();
-            
+
             // Splitting into fragments: fragments with even indices are outside of math area
             string[] textFragments = Regex.Split(text, @"(?=\$\$|[^\$]\$[^\$])|(?=\\begin{equation}|\\end{equation})");
-            
+
             // Test 
             var text2 = string.Join("", textFragments);
-            if (text==text2)
+            if (text == text2)
             {
                 Console.Write("");
             }
@@ -82,8 +125,7 @@ namespace TextProcessor
             text = Regex.Replace(text, @"(\r?\n\s*){2,}\\end{equation}", "\r\n\\end{equation}");
             // Removing redundant newlines after \begin{equation}
             text = Regex.Replace(text, @"\\begin{equation}(?<label>\\label{.*?})?(\r?\n\s*){2,}", "\\begin{equation}${label}\r\n");
-
-            File.WriteAllText(destinationFilename, text, encoding);
+            return text;
         }
     }
 }
